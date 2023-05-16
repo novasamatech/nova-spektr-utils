1acca9e1a543dcce78f501f1b71042dcfa86b5a8
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2023-05-16 15:20:46 +0300

    Adding auto-updating script (#3)
    * Add logic
    
    * Update icons
    
    * update icons
    
    * adding new tokens
    
    * imrpove implementation
    
    * generate new files
    
    * refactoring
    
    * Change wbtc icons
    
    * Adding CI
    
    * fix path in set env
    
    * fix action
    
    * test
    
    * test
    
    * remove data back
    
    * fix typo
    
    * use npm instad of pnpm
    
    * fix command
    
    * update env implementation
    
    * update branch
    
    * fix default case
    
    * Update check chains file
    
    * script for replacement space on underscore
    
    * workflow updates
    
    * remove direct replacement in order to support space in URLs
    
    * Remove dependency on version from check script
    
    * update files, based on the last changes
    
    * rewrite if condition on callback
    
    * fix naming
    
    * remove broken files
    
    * replace with right icons
    
    ---------
    
    Co-authored-by: pgolovkin <golovkin.pl@gmail.com>

---
+++ scripts/checkChainsConfig.js
@@ -0,0 +1,135 @@
+const fs = require('fs');
+const jp = require('jsonpath');
+const path = require('path');
+
+
+const BASE_ICON_PATH = "/nova-spektr-utils/main/icons/"
+
+let hasError = false;
+function checkChainsFile(filePath) {
+    let chainsFile = fs.readFileSync(filePath);
+    let chainsJSON = JSON.parse(chainsFile);
+    let allIcons = jp.query(chainsJSON, "$..icon");
+    let relativeIcons = [];
+    for (let i in allIcons) {
+        relativeIcons.push('.' + allIcons[i].substring(allIcons[i].indexOf('/icons/')))
+    }
+
+    let badPath = new Set();
+    for (i in relativeIcons) {
+        let path = relativeIcons[i];
+        try {
+            fs.readFileSync(path);
+        } catch (error) {
+            badPath.add(path);
+        }
+    }
+    if (badPath.size > 0) {
+        console.error("No icons for chains or assets in " + filePath);
+        console.log(badPath);
+        hasError = true;
+    } else {
+        console.log("All icons found in in " + filePath);
+    }
+
+    let assetIcons = jp.query(chainsJSON, "$..assets[*].icon");
+    let badAssetIcon = new Set();
+    for (let i in assetIcons) {
+        if (assetIcons[i].indexOf(`${BASE_ICON_PATH}`) === -1) {
+            badAssetIcon.add(assetIcons[i]);
+        }
+        if (assetIcons[i].indexOf(`/assets/white/`) === -1) {
+            badAssetIcon.add(assetIcons[i]);
+        }
+    }
+    if (badAssetIcon.size > 0) {
+        console.error("Bad asset icons paths in " + filePath);
+        console.log(badAssetIcon);
+        hasError = true;
+    } else {
+        console.log("All asset icons path is correct in " + filePath);
+    }
+
+    let chainIcons = jp.query(chainsJSON, "$[*].icon");
+    let badChainIcons = new Set();
+    for (let i in chainIcons) {
+        if (chainIcons[i].indexOf(`${BASE_ICON_PATH}`) === -1) {
+            badChainIcons.add(chainIcons[i]);
+        }
+        if (chainIcons[i].indexOf(`/chains/`) === -1) {
+            badChainIcons.add(chainIcons[i]);
+        }
+    }
+    if (badChainIcons.size > 0) {
+        console.error("Bad chain icons paths in " + filePath);
+        console.log(badChainIcons);
+        hasError = true;
+    } else {
+        console.log("All chain icons path is correct in " + filePath);
+    }
+
+    let buyProviders = jp.query(chainsJSON, "$..buyProviders");
+    if (buyProviders.length > 0) {
+        console.error("Buy providers has to be excluded from " + filePath);
+        console.log(buyProviders);
+        hasError = true;
+    }
+    let chainOptions = jp.query(chainsJSON, "$..options[?(@ != 'testnet')]");
+    if (chainOptions.length > 0) {
+        console.error("Chain options has to be removed from " + filePath);
+        console.log(chainOptions);
+        hasError = true;
+    }
+    let chainTypes = jp.query(chainsJSON, "$..types");
+    if (chainTypes.length > 0) {
+        console.error("Chain types has to be removed from " + filePath);
+        console.log(chainTypes);
+        hasError = true;
+    }
+}
+
+function traverseDir(dirPath, checkFunction, callback) {
+    fs.readdir(dirPath, function (err, files) {
+        if (err) {
+            console.error('Error while reading directory:', err);
+            return callback(err);
+        }
+
+        let pending = files.length;
+
+        if (!pending) return callback(null);
+
+        files.forEach(function (file) {
+            const fullPath = path.join(dirPath, file);
+            fs.stat(fullPath, function (err, stats) {
+                if (err) {
+                    console.error('Error while getting file stats:', err);
+                    return callback(err);
+                }
+
+                if (stats.isDirectory()) {
+                    // Recursive call in order to support nested directories
+                    traverseDir(fullPath, checkFunction, function (err) {
+                        if (!--pending) callback(err);
+                    });
+                } else {
+                    checkFunction(fullPath);
+                    if (!--pending) callback(null);
+                }
+            });
+        });
+    });
+}
+
+traverseDir(path.join(__dirname, '../chains/'), checkChainsFile, function (err) {
+    if (err) {
+        throw new Error('Error while traversing directory:', err);
+    } else {
+        // All files and directories have been processed
+        if (hasError) {
+            throw new Error('Some chains file has problems with path, check the log');
+        } else {
+            console.log('All files processed successfully');
+        }
+    }
+});
