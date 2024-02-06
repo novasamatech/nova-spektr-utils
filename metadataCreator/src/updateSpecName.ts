11b7723c6b75f951c9c5da23128a63b5521a160b
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2024-02-06 13:41:51 +0300

    update specName definition (#105)
    * update specName definition
    
    * fix comment
    
    * fix unnecessary undefined

--- metadataCreator/src/updateSpecName.ts
+++ metadataCreator/src/updateSpecName.ts
@@ -1,6 +1,12 @@
 import fs from "fs";
 import { Chain } from "./models/Chain";
 
+const BASE_RELAYCHAINS: { [key: string]: string } = {
+    '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3': 'polkadot',
+    '0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe': 'kusama',
+    '0xe143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e': 'westend'
+};
+
 async function updateSpecName(url: string) {
     try {
         const jsonData = fs.readFileSync(url);
@@ -18,29 +24,33 @@ async function updateSpecName(url: string) {
                     chain.icon,
                     chain.addressPrefix,
                     chain.externalApi,
-                    chain.explorers
+                    chain.explorers,
+                    chain.parentId
                 )
             );
         });
-
-        const newJson = await Promise.all(
-            chains.map(async (chain, index) => {
+        const newJson = await Promise.all(chains.map(async (chain, index) => {
+            let specName: string | undefined;
+            if (BASE_RELAYCHAINS.hasOwnProperty(chain.chainId)) {
+                specName = BASE_RELAYCHAINS[chain.chainId];
+            } else if (chain.parentId && BASE_RELAYCHAINS.hasOwnProperty(chain.parentId)) {
+                specName = BASE_RELAYCHAINS[chain.parentId];
+            } else {
                 await chain.createAPI();
-                const specName = chain.api?.runtimeVersion.specName.toString();
-        
-                return specName
-                    ? { 
-                        ...Object.keys(jsonChains[index]).reduce((obj, key) => {
-                            if (key === 'name') {
-                                return { ...obj, [key]: jsonChains[index][key], specName };
-                            } else {
-                                return { ...obj, [key]: jsonChains[index][key] };
-                            }
-                        }, {}),
-                    }
-                    : { ...jsonChains[index] };
-            })
-        );
+                specName = chain.api?.runtimeVersion.specName.toString();
+            }
+            return specName
+                ? {
+                    ...Object.keys(jsonChains[index]).reduce((obj, key) => {
+                        if (key === 'name') {
+                            return { ...obj, [key]: jsonChains[index][key], specName };
+                        } else {
+                            return { ...obj, [key]: jsonChains[index][key] };
+                        }
+                    }, {}),
+                }
+                : { ...jsonChains[index] };
+        }));
 
         // Corrected file write using string concatenation
         try {
