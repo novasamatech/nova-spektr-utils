c0121d84b9fc3e995e16bb0571e8b8801bb7f9b2
asmadek <alma2610@ya.ru>
2024-02-28 13:04:05 +0500

    feat: support evm chains

--- scripts/buildChainsJson.js
+++ scripts/buildChainsJson.js
@@ -55,8 +55,14 @@ multisigLines.forEach(line => {
 const regularProxies = [
   "0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3", // Polkadot
   "0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe", // Kusama
-  "0xfe58ea77779b7abda7da4ec526d14db9b1e9cd40a217c34892af80a9b332b76d", // Moonbeam (not supported now)
+  "0xfe58ea77779b7abda7da4ec526d14db9b1e9cd40a217c34892af80a9b332b76d", // Moonbeam
   "0xe143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e" // Westend
+]
+
+const evmChains = [
+  "0xfe58ea77779b7abda7da4ec526d14db9b1e9cd40a217c34892af80a9b332b76d", // Moonbeam
+  "0x401a1f9dca3da46f5c4091016c8a2f26dcea05865116b286f60f668207d1474b", // Moonriver
+  "0x91bc6e169807aaa54802737e1c504b2577d4fafedd5a02c10293b1cd60e39527", // Moonbase alpha
 ]
 
 async function getDataViaHttp(url, filePath) {
@@ -101,12 +107,12 @@ function fillAssetData(chain) {
 
 function getTransformedData(rawData) {
   const filteredData = rawData.filter(chain => {
-    const isEthereumBased = chain.options?.includes('ethereumBased');
+    const isSupportedEvmChain = evmChains.some((chainId) => chainId.includes(chain.chainId));
     const isExcludedChain = chain.chainId in EXCLUDED_CHAINS;
     const isPausedChain = chain.name.includes('PAUSE');
 
 
-    return !isEthereumBased && !isExcludedChain && !isPausedChain;
+    return isSupportedEvmChain && !isExcludedChain && !isPausedChain;
   });
 
   return filteredData.map(chain => {
@@ -119,6 +125,11 @@ function getTransformedData(rawData) {
       if (multisigMap[chain.name]) {
         options = [...(options || []), 'multisig'];
       }
+
+      if (chain.options?.includes("ethereumBased")) {
+        options = ["ethereum_based"];
+      }
+
 
       if (regularProxies.some((chainId) => chainId.includes(chain.chainId))) {
         options = [...(options || []), 'regular_proxy'];
