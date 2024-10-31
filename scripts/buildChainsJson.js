00b4b44698c0698e096fd4bb65ca35ae99a6ad0a
Yaroslav Grachev <tuulwq@gmail.com>
2024-10-31 10:28:31 +0300

    Feat: Type_native, Save wnd for prod (#180)
    * feat: type_native, save wnd for prod
    
    * chore: rollback env
    
    * fix: symbol error

--- scripts/buildChainsJson.js
+++ scripts/buildChainsJson.js
@@ -1,11 +1,13 @@
 const path = require('path');
-const { writeFile, readFile } = require('fs/promises');
+const {writeFile, readFile} = require('fs/promises');
 const fs = require('fs');
-const axios = require('axios');
 
 const TOKEN_NAMES = require('./data/assetsNameMap.json');
 const TICKER_NAMES = require('./data/assetTickerMap.json');
 const PROXY_LIST = require('./data/proxyList.json');
+
+const EXCEPTIONAL_CHAINS = require('./data/exceptionalChains.json');
+const ALLOWED_CHAINS = require('./data/allowedChains.json');
 
 const NOVA_CONFIG_VERSION = process.env.NOVA_CONFIG_VERSION;
 const SPEKTR_CONFIG_VERSION = process.env.SPEKTR_CONFIG_VERSION;
@@ -14,26 +16,25 @@ const NOVA_CONFIG_URL = `https://raw.githubusercontent.com/novasamatech/nova-uti
 const ASSET_ICONS_DIR = `icons/v1/assets/white`
 
 const CHAINS_ENV = ['chains_dev.json', 'chains.json'];
-const ALLOWED_CHAINS = require('./data/allowedChains.json');
 
 const TYPE_EXTRAS_REPLACEMENTS = {
-    'baf5aabe40646d11f0ee8abbdc64f4a4b7674925cba08e4a05ff9ebed6e2126b':   'AcalaPrimitivesCurrencyCurrencyId',
-    'fc41b9bd8ef8fe53d58c7ea67c794c7ec9a73daf05e6d54b14ff6342c99ba64c':   'AcalaPrimitivesCurrencyCurrencyId',
-    '262e1b2ad728475fd6fe88e62d34c200abe6fd693931ddad144059b1eb884e5b':   'BifrostPrimitivesCurrencyCurrencyId',
-    '9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed':   'BifrostPrimitivesCurrencyCurrencyId',
-    'f22b7850cdd5a7657bbfd90ac86441275bbc57ace3d2698a740c7b0ec4de5ec3':   'BitCountryPrimitivesFungibleTokenId',
-    'bf88efe70e9e0e916416e8bed61f2b45717f517d7f3523e33c7b001e5ffcbc72':   'InterbtcPrimitivesCurrencyId',
-    '3a5a5cd27eb15fd26c37315a0f0b938733bb798c897428448efac5e6150cad06':   'InterbtcPrimitivesCurrencyId',
-    '418ae94c9fce02b1ab3b5bc211cd2f2133426f2861d97482bbdfdac1bbb0fb92':   'InterbtcPrimitivesCurrencyId',
-    '9af9a64e6e4da8e3073901c3ff0cc4c3aad9563786d89daf6ad820b6e14a0b8b':   'InterbtcPrimitivesCurrencyId',
-    'cceae7f3b9947cdb67369c026ef78efa5f34a08fe5808d373c04421ecf4f1aaf':   'SpacewalkPrimitivesCurrencyId',
-    '5d3c298622d5634ed019bf61ea4b71655030015bde9beb0d6a24743714462c86':   'SpacewalkPrimitivesCurrencyId'
+  'baf5aabe40646d11f0ee8abbdc64f4a4b7674925cba08e4a05ff9ebed6e2126b': 'AcalaPrimitivesCurrencyCurrencyId',
+  'fc41b9bd8ef8fe53d58c7ea67c794c7ec9a73daf05e6d54b14ff6342c99ba64c': 'AcalaPrimitivesCurrencyCurrencyId',
+  '262e1b2ad728475fd6fe88e62d34c200abe6fd693931ddad144059b1eb884e5b': 'BifrostPrimitivesCurrencyCurrencyId',
+  '9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed': 'BifrostPrimitivesCurrencyCurrencyId',
+  'f22b7850cdd5a7657bbfd90ac86441275bbc57ace3d2698a740c7b0ec4de5ec3': 'BitCountryPrimitivesFungibleTokenId',
+  'bf88efe70e9e0e916416e8bed61f2b45717f517d7f3523e33c7b001e5ffcbc72': 'InterbtcPrimitivesCurrencyId',
+  '3a5a5cd27eb15fd26c37315a0f0b938733bb798c897428448efac5e6150cad06': 'InterbtcPrimitivesCurrencyId',
+  '418ae94c9fce02b1ab3b5bc211cd2f2133426f2861d97482bbdfdac1bbb0fb92': 'InterbtcPrimitivesCurrencyId',
+  '9af9a64e6e4da8e3073901c3ff0cc4c3aad9563786d89daf6ad820b6e14a0b8b': 'InterbtcPrimitivesCurrencyId',
+  'cceae7f3b9947cdb67369c026ef78efa5f34a08fe5808d373c04421ecf4f1aaf': 'SpacewalkPrimitivesCurrencyId',
+  '5d3c298622d5634ed019bf61ea4b71655030015bde9beb0d6a24743714462c86': 'SpacewalkPrimitivesCurrencyId'
 }
 const STAKING_ALLOWED_ARRAY = ['Polkadot', 'Kusama', 'Westend', 'Polkadex', 'Ternoa', 'Novasama Testnet - Governance']
 
 const GOV_ALLOWED_ARRAY = ['Polkadot', 'Kusama', 'Westend', 'Rococo', 'Novasama Testnet - Governance'];
 
-const DEFAULT_ASSETS = ['SHIBATALES', 'DEV', 'SIRI', 'PILT', 'cDOT-6/13', 'cDOT-7/14', 'cDOT-8/15', 'cDOT-9/16', 'cDOT-10/17', 'TZERO', 'UNIT', 'Unit', 'tEDG','JOE', 'HOP', 'PAS'];
+const DEFAULT_ASSETS = ['SHIBATALES', 'DEV', 'SIRI', 'PILT', 'cDOT-6/13', 'cDOT-7/14', 'cDOT-8/15', 'cDOT-9/16', 'cDOT-10/17', 'TZERO', 'UNIT', 'Unit', 'tEDG', 'JOE', 'HOP', 'PAS'];
 
 const readmeContent = fs.readFileSync('chains/v1/README.md', 'utf8');
 const multisigSection = readmeContent.split('# List of Networks where we are support Multisig pallet')[1].split('## The list of supported networks')[0];
@@ -55,123 +56,90 @@ const evmChains = [
   "0x91bc6e169807aaa54802737e1c504b2577d4fafedd5a02c10293b1cd60e39527", // Moonbase alpha
 ]
 
-async function getDataViaHttp(url, filePath) {
-  try {
-    const response = await axios.get(url + filePath);
-
-    return response.data;
-  } catch (error) {
-    console.log('Error: ', error?.message || 'getDataViaHttp failed');
-  }
+function getDataFromUrl(url, filePath) {
+  return fetch(url + filePath, {method: 'GET'})
+    .then(response => response.json())
+    .catch(error => console.log('Error: ', error?.message || 'getDataViaHttp failed'));
 }
 
 function getStakingValue(staking, chainName) {
-  if (STAKING_ALLOWED_ARRAY.includes(chainName)) {
-    return Array.isArray(staking) ? staking[0] : typeof staking === 'string' ? staking : undefined;
+  if (!STAKING_ALLOWED_ARRAY.includes(chainName)) {
+    return undefined;
   }
-  return undefined;
+
+  return Array.isArray(staking) ? staking[0] : typeof staking === 'string' ? staking : undefined;
 }
 
 function fillAssetData(chain) {
-  const assetsList = [];
-  chain.assets.map(asset => {
+  return chain.assets.map(asset => {
     // Skip assets with typeExtras.palletName: "ForeignAssets"
-    if (asset.typeExtras?.palletName === "ForeignAssets") {
-      return;
-    }
-
+    if (asset.typeExtras?.palletName === "ForeignAssets") return;
     // Temp remove, waiting for "AssetManagement pallet support. It's used by Zeitgeist." https://app.clickup.com/t/85ztgpy7n
-    if (chain.name === 'Zeitgeist' && asset.symbol === 'DOT') {
-      return;
-    }
-    if (asset.symbol.endsWith('.s')) {
-      return;
-    }
+    if (chain.name === 'Zeitgeist' && asset.symbol === 'DOT') return;
+    if (asset.symbol.endsWith('.s')) return;
     // Remove Pool tokens
-    if (asset.symbol.includes('Pool')) {
-      return;
-    }
+    if (asset.symbol.includes('Pool')) return;
     // Remove LP tokens
-    if (asset.symbol.startsWith('LP ')) {
-      return;
-    }
+    if (asset.symbol.startsWith('LP ')) return;
     // Remove Special DOT tokens
-    if (asset.symbol.startsWith('cDOT')) {
-      return;
-    }
-
+    if (asset.symbol.startsWith('cDOT')) return;
     // Remove Snowbridge tokens
-    if (asset.symbol.endsWith('-Snowbridge')) {
-      return;
-    }
+    if (asset.symbol.endsWith('-Snowbridge')) return;
 
-    // Replace (old) in symbols
-    asset.symbol = asset.symbol.replace(/ \(old\)/gi, '');
+    const symbol = asset.symbol.replace(/[_ ]+\(old\)/gi, '')
 
-    assetsList.push({
+    return {
+      name: TOKEN_NAMES[symbol] || 'Should be included in scripts/data/assetsNameMap',
       assetId: asset.assetId,
-      symbol: asset.symbol,
+      symbol,
       precision: asset.precision,
-      type: asset.type,
+      type: asset.type || 'native',
       priceId: asset.priceId,
       staking: getStakingValue(asset.staking, chain.name),
-      icon: replaceUrl(asset.icon, 'asset', asset.symbol),
+      icon: replaceUrl(asset.icon, 'asset', symbol),
       typeExtras: replaceTypeExtras(asset.typeExtras, chain.chainId),
-      name: TOKEN_NAMES[asset.symbol] || 'Should be included in scripts/data/assetsNameMap',
-    });
+    };
   });
-  return assetsList;
 }
 
-
-function getTransformedData(rawData) {
-  const filteredData = rawData.filter(chain => {
-    const isAllowedChain = chain.chainId in ALLOWED_CHAINS;
-    return isAllowedChain;
-  });
+function getPreparedChains(rawData) {
+  const filteredData = rawData.filter(chain => chain.chainId in ALLOWED_CHAINS);
 
   return filteredData.map(chain => {
-    let externalApi = filterObjectByKeys(chain.externalApi, ['staking', 'history', 'governance-delegations']);
     const options = [];
 
     if (chain.options?.includes('testnet')) {
       options.push('testnet');
     }
-
     if (multisigMap[chain.name]) {
       options.push('multisig');
     }
-
     if (chain.options?.includes("ethereumBased")) {
       options.push('ethereum_based');
     }
-
     if (GOV_ALLOWED_ARRAY.includes(chain.name)) {
       options.push('governance');
     }
-
-    const explorers = chain.explorers?.map(explorer => {
-      if (explorer.name === 'Subscan') {
-        const accountParam = explorer.account;
-        const domain = accountParam.substring(0, accountParam.indexOf('account'));
-        return {
-          ...explorer,
-          multisig: `${domain}multisig_extrinsic/{index}?call_hash={callHash}`
-        };
-      }
-
-      return explorer;
-    });
-
     const proxyData = PROXY_LIST.find(p => p.chainId.includes(chain.chainId));
-
     if (proxyData) {
       options.push(...proxyData.options)
     }
-
+    let externalApi = filterObjectByKeys(chain.externalApi, ['staking', 'history', 'governance-delegations']);
     if (proxyData?.externalApi) {
-      externalApi = { ...externalApi, ...proxyData.externalApi }
+      externalApi = {...externalApi, ...proxyData.externalApi}
     }
+
+    const explorers = chain.explorers?.map(explorer => {
+      if (explorer.name !== 'Subscan') return explorer
+
+      const accountParam = explorer.account;
+      const domain = accountParam.substring(0, accountParam.indexOf('account'));
+
+      return {
+        ...explorer,
+        multisig: `${domain}multisig_extrinsic/{index}?call_hash={callHash}`
+      };
+    });
 
     const assets = fillAssetData(chain)
     const nodes = chain.nodes.filter(node => !node.url.includes('{')).map(node => ({
@@ -186,7 +154,7 @@ function getTransformedData(rawData) {
       parentId: chain.parentId ? `0x${chain.parentId}` : undefined,
       icon: replaceUrl(chain.icon, 'chain'),
       options,
-      nodes: nodes,
+      nodes,
       assets,
       explorers,
     };
@@ -227,7 +195,7 @@ function replaceUrl(url, type, name = undefined) {
       const tickerNames = [processedName, processedName.split("-")[0], TICKER_NAMES[processedName]];
       const relativePath = findFileByTicker(tickerNames, ASSET_ICONS_DIR);
       if (!relativePath) {
-        console.error(`Can't find file for: ${processedName} in: ${ASSET_ICONS_DIR}`);
+        console.warn(`Can't find file for: ${processedName} in: ${ASSET_ICONS_DIR}. Trying fallback with customAssetMappings`);
         // Fallback to TICKER_NAMES using original name if processedName fails
         const mappedName = customAssetMappings[processedName] || TICKER_NAMES[name] || processedName;
         return changedBaseUrl.replace(/\/icons\/.*/, `/icons/v1/assets/white/${mappedName}.svg`);
@@ -242,6 +210,7 @@ function replaceUrl(url, type, name = undefined) {
 function replaceTypeExtras(typeExtras, chainId) {
   if (typeExtras && typeExtras.currencyIdType) {
     const replacement = TYPE_EXTRAS_REPLACEMENTS[chainId];
+
     if (replacement) {
       typeExtras.currencyIdType = replacement;
     }
@@ -269,9 +238,9 @@ function findFileByTicker(tickers, dirPath) {
       const byMappedName = new RegExp(`^${mappedName}.svg\\b|\\(${mappedName}\\)\\.svg`, "i");
 
       if (
-          currentFile.match(byFullName)
-          || currentFile.match(byShortName)
-          || currentFile.match(byMappedName)
+        currentFile.match(byFullName)
+        || currentFile.match(byShortName)
+        || currentFile.match(byMappedName)
       ) {
         return path.join(dirPath, currentFile);
       }
@@ -319,7 +288,7 @@ async function saveNewFile(newJson, file_name) {
 
       if (existingItemIndex >= 0) {
         // If item already exists, update it
-        mergedData[existingItemIndex] = { ...mergedData[existingItemIndex], ...newItem };
+        mergedData[existingItemIndex] = {...mergedData[existingItemIndex], ...newItem};
       } else {
         // If item doesn't exist, add it
         mergedData.push(newItem);
@@ -334,25 +303,28 @@ async function saveNewFile(newJson, file_name) {
 }
 
 async function buildFullChainsJSON() {
-  const requests = CHAINS_ENV.map(async (chain) => {
-    try {
-      const novaChainsConfig = await getDataViaHttp(NOVA_CONFIG_URL, chain);
-      const modifiedData = await getTransformedData(novaChainsConfig);
-      await saveNewFile(modifiedData, chain);
-      console.log('❇️ Successfully generated for: ' + chain);
-    } catch (e) {
-      console.error('️🔴 Error for: ', chain, e);
-      process.exit(1);
-    }
+  const requests = CHAINS_ENV.map((fileName) => {
+    return getDataFromUrl(NOVA_CONFIG_URL, fileName).then(getPreparedChains);
   });
 
-  await Promise.allSettled(requests);
+  const [dev, prod] = await Promise.allSettled(requests);
+
+  if (prod.status === 'rejected' || dev.status === 'rejected') {
+    console.log(prod.reason || dev.reason);
+    throw new Error('️🔴 Error preparing DEV or PROD file');
+  }
+
+  const toProd = dev.value.filter(chain => EXCEPTIONAL_CHAINS.prod[chain.chainId]);
+  prod.value.push(...toProd);
+
+  await saveNewFile(dev.value, CHAINS_ENV[0]);
+  await saveNewFile(prod.value, CHAINS_ENV[1]);
+  console.log('❇️ Successfully generated CHAINS for DEV & PROD');
+  console.log('⚠️ Exceptional PROD chains - ', toProd.map(c => c.name));
 }
 
-buildFullChainsJSON()
-  .then(() => console.log('🏁 buildFullChainsJSON finished'))
-  .catch(e => {
-    console.error('🔴 Error in buildFullChainsJSON: ', e);
-    process.exit(1);
-  });
+buildFullChainsJSON().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
 
--- scripts/updateAssetsName.js
+++ scripts/updateAssetsName.js
@@ -5,12 +5,12 @@ const assetsNameMap = JSON.parse(fs.readFileSync('scripts/data/assetsNameMap.jso
 const chainsDev = JSON.parse(fs.readFileSync('chains/v1/chains_dev.json', 'utf8'));
 
 chainsDev.forEach(chain => {
-    chain.assets?.forEach(asset => {
-        if (asset.name === "Should be included in scripts/data/assetsNameMap") {
-            const symbol = asset.symbol;
-            assetsNameMap[symbol] = symbol;
-        }
-    });
+  chain.assets?.forEach(asset => {
+    if (asset.name === "Should be included in scripts/data/assetsNameMap") {
+      const symbol = asset.symbol;
+      assetsNameMap[symbol] = symbol;
+    }
+  });
 });
 
 fs.writeFileSync('scripts/data/assetsNameMap.json', JSON.stringify(assetsNameMap, null, 2), 'utf8');
